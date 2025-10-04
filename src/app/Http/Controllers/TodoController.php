2e54ccdbfe54a820d84db76b5dbf9fe1daab8def
yudai ishige <daidai.yudai827@gmail.com>
2025-10-04 18:09:33 +0900

    変更

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -21,4 +21,12 @@ class TodoController extends Controller
 
          return redirect('/')->with('message', 'Todoを作成しました');
      }
+     public function update(TodoRequest $request)
+{
+    $todo = $request->only(['content']);
+    Todo::find($request->id)->update($todo);
+
+    return redirect('/')->with('message', 'Todoを更新しました');
+}
+
 }
