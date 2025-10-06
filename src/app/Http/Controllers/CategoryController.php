dd37010deb7a4be2354433399bf568cf9c939768
yudai ishige <daidai.yudai827@gmail.com>
2025-10-06 22:15:21 +0900

    変更

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -13,4 +13,13 @@ class CategoryController extends Controller
 
          return view('category', compact('categories'));
      }
+
+     public function store(Request $request)
+{
+  $category = $request->only(['name']);
+  Category::create($category);
+
+  return redirect('/categories')->with('message', 'カテゴリを作成しました');
+}
+
 }
