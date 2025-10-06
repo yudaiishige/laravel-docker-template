5e5f2f41c48e0b0e6fa0ea23b5cedc683c81231c
yudai ishige <daidai.yudai827@gmail.com>
2025-10-06 22:29:16 +0900

    変更

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -28,36 +28,36 @@
     <input class="create-form__item-input" type="text" name="name">
     </div>
     <div class="create-form__button">
-      <button class="create-form__button-submit" type="submit">作成</button>
+    <button class="create-form__button-submit" type="submit">作成</button>
     </div>
   </form>
   <div class="category-table">
     <table class="category-table__inner">
-      <tr class="category-table__row">
+    <tr class="category-table__row">
         <th class="category-table__header">category</th>
-      </tr>
-      @foreach ($categories as $category)
-      <tr class="category-table__row">
+    </tr>
+    @foreach ($categories as $category)
+    <tr class="category-table__row">
         <td class="category-table__item">
-          <form class="update-form">
+        <form class="update-form">
             <div class="update-form__item">
             <input class="update-form__item-input" type="text" value="{{ $category['name'] }}">
             </div>
             <div class="update-form__button">
-              <button class="update-form__button-submit" type="submit">更新</button>
+            <button class="update-form__button-submit" type="submit">更新</button>
             </div>
-          </form>
+        </form>
         </td>
         <td class="category-table__item">
-          <form class="delete-form">
+        <form class="delete-form">
             <div class="delete-form__button">
-              <button class="delete-form__button-submit" type="submit">削除</button>
+            <button class="delete-form__button-submit" type="submit">削除</button>
             </div>
-          </form>
+        </form>
         </td>
-      </tr>
-      @endforeach
+    </tr>
+    @endforeach
     </table>
-  </div>
+　</div>
 </div>
 @endsection
