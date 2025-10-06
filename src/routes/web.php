67b2510e67ca841c98811d149d45cb9d486d8a40
yudai ishige <daidai.yudai827@gmail.com>
2025-10-06 22:21:52 +0900

    変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
 Route::patch('/todos/update', [TodoController::class, 'update']);
 Route::delete('/todos/delete', [TodoController::class, 'destroy']);
 Route::get('/categories', [CategoryController::class, 'index']);
