497ae51014b6f6f4f83e26f6ea146d0731da732e
yudai ishige <daidai.yudai827@gmail.com>
2025-10-06 22:19:40 +0900

    変更

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -15,11 +15,11 @@ class CategoryController extends Controller
      }
 
      public function store(Request $request)
-{
+     {
   $category = $request->only(['name']);
   Category::create($category);
 
   return redirect('/categories')->with('message', 'カテゴリを作成しました');
-}
+     }
 
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', [TodoController::class, 'index']);
-Route::post('/todos', [TodoController::class, 'store']);
 Route::patch('/todos/update', [TodoController::class, 'update']);
 Route::delete('/todos/delete', [TodoController::class, 'destroy']);
 Route::get('/categories', [CategoryController::class, 'index']);
