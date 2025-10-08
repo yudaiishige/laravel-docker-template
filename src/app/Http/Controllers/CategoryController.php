1f90458695cc4747a2d80de8ec66e96beebbd8b0
yudai ishige <daidai.yudai827@gmail.com>
2025-10-08 22:02:56 +0900

    変更

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
          return view('category', compact('categories'));
      }
 
-     public function store(Request $request)
+     public function store(CategoryRequest $request)
      {
   $category = $request->only(['name']);
   Category::create($category);
--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -25,7 +25,7 @@
 <form class="create-form" action="/categories" method="post">
     @csrf
     <div class="create-form__item">
-    <input class="create-form__item-input" type="text" name="name">
+    <input class="create-form__item-input" type="text" name="name" value="{{ old('name') }}">
     </div>
     <div class="create-form__button">
     <button class="create-form__button-submit" type="submit">作成</button>
